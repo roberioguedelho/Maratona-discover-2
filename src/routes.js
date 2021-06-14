616af9362a05a3c07047873c18bd10af3faff057
Roberio Guedelho <roberiocrow@gmail.com>
2021-06-14 13:45:19 -0300

    Fim da primeira aula

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const routes = express.Router()
+
+const views = __dirname + "/views/"
+
+const profile = {
+    name: "Robério Guedelho",
+    avatar: "https://avatars.githubusercontent.com/u/68553100?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
+routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
+
+module.exports = routes;
